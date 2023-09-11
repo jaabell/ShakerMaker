* New SRF2 source time function * Fixed gradient issue in computing accelerations * Improved plotting

shakermaker/shakermaker.py
@@ -632,17 +632,15 @@ class ShakerMaker:
                             progress_percent = ipair/npairs*100
                             tnow = perf_counter()
 
-                            if ipair > 0:
-                                time_per_pair = (tnow - tstart)/ipair
-                            else:
-                                time_per_pair = 30
-                            time_left = (npairs - ipair)*time_per_pair
+                            time_per_pair = (tnow - tstart)/(ipair+1)
+
+                            time_left = (npairs - ipair - 1)*time_per_pair
 
                             hh = np.floor(time_left / 3600)
                             mm = np.floor((time_left - hh*3600)/60)
                             ss = time_left - mm*60 - hh*3600
 
-                            print(f"{ipair} of {npairs} ({progress_percent:.4f}%) ETA = {hh}:{mm}:{ss} {t[0]=:0.4f} {t[-1]=:0.4f} ({tmin=:0.4f} {tmax=:0.4f})")
+                            print(f"{ipair} of {npairs} ({progress_percent:.4f}%) ETA = {hh:.0f}:{mm:0f}:{ss} {t[0]=:0.4f} {t[-1]=:0.4f} ({tmin=:0.4f} {tmax=:0.4f})")
 
                 else: 
                     pass

shakermaker/tools/plotting.py
@@ -69,10 +69,18 @@ def ZENTPlot(station, fig=0, show=False, xlim=[], label=[], integrate=0, differe
         elif differentiate > 0 and integrate == 0:
             # z,e,n,t = station.get_response_derivative(ntimes=differentiate)
             # z,e,n,t = station.get_response_derivative(ntimes=differentiate)
-            z,e,n,t = station.get_response()
-            z = np.gradient(z, t)
-            e = np.gradient(e, t)
-            n = np.gradient(n, t)
+            z_,e_,n_,t = station.get_response()
+            #z = np.gradient(z, t)
+            Nt = len(t)
+            dt = t[1] - t[0]
+            z = np.zeros(Nt)
+            z[1:] = (z_[1:] - z_[0:-1])/dt
+            #e = np.gradient(e, t)
+            e = np.zeros(Nt)
+            e[1:] = (e_[1:] - e_[0:-1])/dt
+            #n = np.gradient(n, t)
+            n = np.zeros(Nt)
+            n[1:] = (n_[1:] - n_[0:-1])/dt
         else:
             print(f"Not allowed to pass integrate={integrate} and differentiate={differentiate} simultaneously. ")
             return 0